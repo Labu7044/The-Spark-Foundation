4b542105d9b6ae8e172947fd26c05d6c20d209da
Labanya <labnyanayak@gmailcom>
2021-08-16 14:59:38 +0530

    Initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$keyId = 'rzp_test_fD7fFquy5Ufosg';
+$keySecret = '6CCLD1Ok6zBI9YNJ3QbirGGj';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+
---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form</title>
+    <link rel="stylesheet" href="form1.css">
+</head>
+<body>
+
+    <div>
+    <div class="link"> <form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_Hj8ewKrTM1YY8r" async> </script> </form></div> 
+    </div>    
+</body>
+</html>
